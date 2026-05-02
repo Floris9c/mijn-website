ac63142550101fd9e53ca3528795e3589e0df4bf
Floris9c <floris.cockx@gmail.com>
2026-05-02 16:52:52 +0200

    zo goed als klaar

--- js/locatie.js
+++ js/locatie.js
@@ -1,14 +1,18 @@
-let map = L.map('map').setView([51.26610638893674, 4.701277688472604], 15);
+
+
+let mapP = L.map('map').setView([51.270340, 4.7137111], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+  maxZoom: 14,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapP);
 
-let circle = L.circle([51.26610638893674, 4.701277688472604], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 200
-}).addTo(map);
-circle.bindPopup ("Home")
+// plaats icon.png als marker op de map
+let markerIconP1 = L.icon({
+  iconUrl: './assets/images/Logo.png',
+  iconSize: [120, 60], 
+  iconAnchor: [60, 60],
+  popupAnchor: [0, -60]
+});
+
+L.marker([51.274137, 4.717634], { icon: markerIconP1 }).addTo(mapP);
